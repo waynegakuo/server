fdde9900cdb7fb67ba8dd76eed07260a8d204204
waynegakuo <waynejotham@gmail.com>
2020-07-12 05:27:40 +0300

    Creating a simple express server to receive form data from an Angular app

---
+++ server.js
@@ -0,0 +1,23 @@
+const experess = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PORT = 3000;
+
+const app = experess();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', function (req, res) {
+    res.send('Hello from server')
+});
+
+app.post('/enroll', (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ "message": "Data Received" });
+})
+
+app.listen(PORT, function () {
+    console.log("Server running on localhost: ", PORT);
+})
